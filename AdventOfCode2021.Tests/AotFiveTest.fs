565a39682cf25690c463ebe2287904f3cfffe2e8
angelochecked <angelochecked@gmail.com>
2021-12-11 13:17:18 +0100

    draft aot5 1 and 2 end

--- AdventOfCode2021.Tests/AotFiveTest.fs
+++ AdventOfCode2021.Tests/AotFiveTest.fs
@@ -7,9 +7,7 @@ open AdventOfCode2021.DayFive
 let aotInputFile =
     $"{__SOURCE_DIRECTORY__}/resource/day5.txt"
 
-[<Fact>]
-let ``parse boards`` () =
-    let rawInput = """0,9 -> 5,9
+let rawInput = """0,9 -> 5,9
 8,0 -> 0,8
 9,4 -> 3,4
 2,2 -> 2,1
@@ -19,6 +17,9 @@ let ``parse boards`` () =
 3,4 -> 1,4
 0,0 -> 8,8
 5,5 -> 8,2"""
+
+[<Fact>]
+let ``parse boards`` () =
     let inputLines = rawInput.Split('\n') |> Array.toList
     let output = parseCoos inputLines 
     let expect = [
@@ -36,10 +37,10 @@ let ``parse boards`` () =
     Assert.Equal<List<Area>>(expect, output)
     
 [<Fact>]
-let ``from Area to all coos`` () =
+let ``from Area to all straight coos`` () =
     let area = {From=(6,4); To=(2,0)}
 
-    let coos = allCoo area
+    let coos = allStraightCoo area
 
     let expect = [
         (6,4); (5,4); (4,4); (3,4); (2,4);
@@ -48,19 +49,69 @@ let ``from Area to all coos`` () =
     Assert.Equal<List<int * int>>(expect, coos)
 
 [<Fact>]
-let ``from Area to all coos 2`` () =
+let ``from Area to all straight coos 2`` () =
     let area = {From=(1,1); To=(1,3)}
 
-    let coos = allCoo area
+    let coos = allStraightCoo area
 
     let expect = [ (1,1); (1,2); (1,3); ]
     Assert.Equal<List<int * int>>(expect, coos)
 
 [<Fact>]
-let ``from Area to all coos 3`` () =
+let ``from Area to all straight coos 3`` () =
     let area = {From=(9,7); To=(7,7)}
 
-    let coos = allCoo area
+    let coos = allStraightCoo area
 
     let expect = [ (9,7);(8,7);(7,7); ]
     Assert.Equal<List<int * int>>(expect, coos)
+
+[<Fact>]
+let ``from Area to all straight coos 4`` () =
+    let area = {From=(0,9); To=(5,9)}
+
+    let coos = allStraightCoo area
+
+    let expect = [ (0,9);(1,9);(2,9);(3,9);(4,9);(5,9); ]
+    Assert.Equal<List<int * int>>(expect, coos)
+
+[<Fact>]
+let ``from Area to all diagonal coos`` () =
+    let area = {From=(9,7); To=(7,9)}
+
+    let coos = allDiagonalCoo area
+
+    let expect = [ (9,7);(8,8);(7,9) ]
+    Assert.Equal<List<int * int>>(expect, coos)
+
+[<Fact>]
+let ``from Area to all diagonal coos 2`` () =
+    let area = {From=(1,1); To=(3,3)}
+
+    let coos = allDiagonalCoo area
+
+    let expect = [ (1,1);(2,2);(3,3) ]
+    Assert.Equal<List<int * int>>(expect, coos)
+
+[<Fact>]
+let ``from Area to not diagonal coos`` () =
+    let area = {From=(0,9); To=(5,9)}
+
+    let coos = allDiagonalCoo area
+
+    let expect = [ (0,9);(1,9);(2,9);(3,9);(4,9);(5,9); ]
+    Assert.Equal<List<int * int>>(expect, coos)
+
+[<Fact>]
+let ``Aot5 Solution`` () =
+    let inputLines = read aotInputFile |> Seq.toList
+    let areas = parseCoos inputLines 
+    
+    Assert.Equal(5197, numberOfOverlappingStraightAreas areas)
+
+[<Fact>]
+let ``Aot5 Solution part2`` () =
+    let inputLines = read aotInputFile |> Seq.toList
+    let areas = parseCoos inputLines 
+    
+    Assert.Equal(18605, numberOfOverlappingAllAreas areas)
--- AdventOfCode2021/AotFive.fs
+++ AdventOfCode2021/AotFive.fs
@@ -3,7 +3,7 @@ namespace AdventOfCode2021
 module DayFive =
     type Coo = int * int
     type Area = {From: Coo; To: Coo} 
-    type AreaMap = Map<Area, int>
+    type AreaMap = Map<int * int, int>
 
     let parseCoos (lines:List<string>) =
         lines
@@ -13,11 +13,61 @@ module DayFive =
                                    { From= (from.[0],from.[1]); To=(to_.[0],to_.[1]) }
                    )
 
-    let x(coos:List<Area>) =
-       1 
-
-    let allCoo({From=(x1,y1); To=(x2,y2)}): List<int * int> =
+    let allStraightCoo({From=(x1,y1); To=(x2,y2)}): List<int * int> =
         let xStep = if x1 < x2 then 1 else -1
         let yStep = if y1 < y2 then 1 else -1
         [for x in x1..(xStep)..x2 -> (x, y1) ] @ [for y in y1+(yStep)..(yStep)..y2 -> (x1, y)]
+
+    let overlapAllStraightCoos(coos:List<Area>) =
+        let map: AreaMap = Map []
+        let rec foo map coo =
+            map |> Map.change coo (fun value ->
+               match value with
+               | Some value -> Some (value+1)
+               | None -> Some 1
+            )
         
+        coos 
+        |> List.map allStraightCoo 
+        |> List.concat
+        |> List.fold foo map
+
+    let numberOfOverlappingStraightAreas areas = 
+        let straightCoo = areas |> List.filter(fun { From=(x1,y1); To=(x2,y2) } -> x1 = x2 || y1=y2 ) 
+        overlapAllStraightCoos straightCoo 
+        |> Map.filter(fun _ v -> v > 1)
+        |> Map.toList 
+        |> List.length
+
+    let allDiagonalCoo({From=(x1,y1); To=(x2,y2)}): List<int * int> =
+        let cartesianProduct x y =
+           x |> List.collect (fun xs -> y |> List.map (fun ys -> xs, ys))
+
+        let xStep = if x1 < x2 then 1 else -1
+        let yStep = if y1 < y2 then 1 else -1
+        let x = [ for x in x1..(xStep)..x2 -> x] 
+        let y = [ for y in y1..(yStep)..y2 -> y ] 
+
+        if (List.length x) = (List.length y)
+        then y |> List.zip(x)
+        else cartesianProduct x y 
+
+    let overlapAllDiagonalCoos(coos:List<Area>) =
+        let map: AreaMap = Map []
+        let rec foo map coo =
+            map |> Map.change coo (fun value ->
+               match value with
+               | Some value -> Some (value+1)
+               | None -> Some 1
+            )
+        
+        coos 
+        |> List.map allDiagonalCoo 
+        |> List.concat
+        |> List.fold foo map
+
+    let numberOfOverlappingAllAreas areas = 
+        overlapAllDiagonalCoos areas
+        |> Map.filter(fun _ v -> v > 1)
+        |> Map.toList 
+        |> List.length
